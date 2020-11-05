2f318dc1b64b2bfc5dc1d02f4ac63f50d7d2a517
kelvin <kelvin.colina@intive.com>
2020-11-04 21:30:32 -0300

    Adding the method get to obtain all the contacs

---
+++ contacs.py
@@ -0,0 +1,10 @@
+contacts = [
+    {
+        "id": 1,
+        "name": "Kelvin",
+        "last_name": "Colina",
+        "address": "Buenos Aires",
+        "email": "email@test.com",
+        "phone": "+5491199999999"
+    }
+]
